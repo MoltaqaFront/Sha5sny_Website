6c7a4415213a11457678f2bee30c490793ad5576
shadyaabed <shady@moltaqa.net>
2023-11-26 10:11:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DiagnoseMe_website
---
+++ locales/en-US.js
@@ -0,0 +1,65 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About Shakhsny',
+      title: 'The leading real estate platform',
+      desc: 'We believe in the power of the positive impact of therapeutic prayer on mental and spiritual health. Our team has extensive experience in the field of spiritual healing and information technology, and we work passionately to provide the best possible experience to our clients.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: "Therapeutic prayer",
+      desc: 'The Personalize Me application provides a huge library containing a wide range of spiritual illnesses and various psychological problems. Users can browse the library and search for the disease they suffer from easily.',
+    },
+    {
+      image: '2.svg',
+      title: "Comprehensive database",
+      desc: 'Thanks to high-quality audio technology, users can listen to audio clips of therapeutic supplications associated with each disease, to promote spiritual and psychological healing. We provide a carefully selected dua for each condition, making it easier for clients to access the treatment that is right for them.'
+    },
+    {
+      image: '3.svg',
+      title: "Simplified user experience",
+      desc: "The My Person application features an intuitive and easy-to-use user interface, which makes the process of browsing and searching for diseases and treatments very smooth and easy.",
+    },
+  ],
+  about: {
+    mainTitle: 'Shakhsny app is your ideal choice',
+    title: 'About Shakhsny',
+    desc: "<p>A multi-service online store application that allows users to purchase products and services online and have them delivered to their address. The application also includes reservation options for restaurants and events, and facilitates the process of obtaining the required services in an easy and convenient way. Those who are looking for a modern and sophisticated shopping experience, as it provides many distinctive and convenient services that make online shopping easy and enjoyable. Therefore, the application can be downloaded easily and benefit from all the services available on it with comfort and confidence. Get the best online shopping experience with the Pring application. Enjoy competitive prices and high quality of the products you buy through the Shakhsny app. Fast and reliable delivery for all your purchases, with secure and easy payment options</p>",
+  },
+  downloadText: "<p>You can download the Shakhsny app now by visiting the App Store on your smartphone, searching for Shakhsny, and then downloading the app on your smartphone.</p>",
+  isoLink: "",
+  androidLink: "",
+  address: 'KSA - Jeddah',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved For Moltaqa Software House',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+    },
+}
